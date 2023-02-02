269e679bafe37b41650366ba2edfd1812244e07f
aliharslan0 <97433474+aliharslan0@users.noreply.github.com>
2023-02-02 23:03:24 +0300

    no requirements

--- main.py
+++ main.py
@@ -1,30 +1,63 @@
-from json import dumps
-from os import startfile
-from pathlib import Path
-from subprocess import check_output
-from time import sleep
-from zipfile import ZipFile
+import json
+import os
+import pathlib
+import re
+import socket
+import subprocess
+import time
+import urllib.request
+import zipfile
+from datetime import datetime
 
-from dotenv import dotenv_values, set_key
-from requests import get
-from websocket import create_connection
 
-config = dotenv_values('.env')
+def parse_dotenv(file_path):
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+    return {line.split('=')[0]: line.split('=')[1].strip() for line in lines if line and not line.startswith('#')}
+
+
+config = parse_dotenv('.env')
+pattern = re.compile(r"^[a-zA-Z0-9_]+\.[a-zA-Z0-9-]+\.([a-zA-Z0-9-]+\.)*[a-zA-Z0-9_]+$")
+
+
+def set_key(file_path, key, value, quote_mode='never'):
+    quote = '' if quote_mode == 'never' else "'"
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+
+    with open(file_path, 'w') as f:
+        found = False
+        for line in lines:
+            if line.startswith(f"{key}="):
+                f.write(f"{key}={quote}{value}{quote}\n")
+                found = True
+            else:
+                f.write(line)
+        if not found:
+            f.write(f"{key}={quote}{value}{quote}\n")
+
+
+def get_m_data():
+    input_string = input('mData: ')
+    if re.match(pattern, input_string):
+        return input_string
+    else:
+        return None
 
 
 def update_loader():
-    url = get(config['UPDATE_URL']).text
+    url = urllib.request.urlopen('https://raw.githubusercontent.com/sbyteui-bot/ldr/main/data/link.txt').read().decode()
     zip_path = url.split('/')[-1]
     if zip_path == config['ZIP_PATH']:
         return False
 
     with open(zip_path, 'wb') as file:
-        for chunk in get(url, stream=True).iter_content():
-            file.write(chunk)
+        content = urllib.request.urlopen(url).read()
+        file.write(content)
 
-    old = Path(config['ZIP_PATH'])
-    if old.is_file():
-        old.unlink(False)
+    old_path = pathlib.Path(config['ZIP_PATH'])
+    if old_path.is_file():
+        old_path.unlink()
 
     set_key('.env', 'ZIP_PATH', zip_path, quote_mode='never')
     config['ZIP_PATH'] = zip_path
@@ -34,23 +67,43 @@ def update_loader():
 def main():
     update_loader()
 
-    with ZipFile(config['ZIP_PATH']) as zf:
+    with zipfile.ZipFile(config['ZIP_PATH']) as zf:
         zf.extractall(pwd=b'mh')
-        startfile(zf.filelist[0].filename)
+        os.startfile(zf.filelist[0].filename)
 
-    timeout = int(config['TIMEOUT'])
+    timeout = 10
+    process_found = False
     while timeout > 0:
-        out = check_output(['TASKLIST', '/FI', f'imagename eq {config["PROCESS_NAME"]}'])
-        if out.splitlines()[-1].startswith(config["PROCESS_NAME"].encode()):
+        out = subprocess.check_output(['TASKLIST', '/FI', 'imagename eq gtnszz.exe'])
+        if b'gtnszz.exe' in out:
+            process_found = True
             break
         timeout -= 1
-        sleep(1)
-    else:
+        time.sleep(1)
+
+    if not process_found:
         raise ProcessLookupError('Process not found.')
 
-    ws = create_connection('ws://localhost:9002')
-    ws.recv()
-    data = {
+    current_time = datetime.now()
+    last_run = datetime.fromisoformat(config['LAST_RUN']) if config['LAST_RUN'] else current_time
+    update_mdata = False
+    if len(config['MDATA']) != 428 or (current_time - last_run).total_seconds() > 3600:
+        config['MDATA'] = get_m_data()
+        config['LAST_RUN'] = current_time.isoformat()
+        update_mdata = True
+
+    for i in range(10):
+        try:
+            ws = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            ws.settimeout(1)
+            ws.connect(("localhost", 9002))
+            break
+        except (ConnectionRefusedError, socket.timeout) as e:
+            if i == 9:
+                raise e
+
+    ws.recv(1024)
+    payload = {
         'method': 'load',
         'json': {
             'token': config['MDATA'],
@@ -58,9 +111,17 @@ def main():
             'id': config['ID']
         }
     }
-    data['json'] = dumps(data['json'])
-    json_str = dumps(data)
-    ws.send(json_str)
+
+    payload['json'] = json.dumps(payload['json'])
+    json_str = json.dumps(payload)
+
+    ws.send(json_str.encode("utf-8"))
+
+    ws.close()
+
+    if update_mdata:
+        set_key('.env', 'MDATA', config['MDATA'], quote_mode='never')
+        set_key('.env', 'LAST_RUN', config['LAST_RUN'], quote_mode='never')
 
 
 if __name__ == '__main__':
